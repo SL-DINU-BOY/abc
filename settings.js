8d9a58b4b3ea943c64867ca3ad649a570b1bbd9d
SL-DINU-BOY <139786264+SL-DINU-BOY@users.noreply.github.com>
2025-08-23 21:45:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=VR41GIhR#8enH36ZGT_3Q8-y7oLtcJyqSyllS4MPy1cnwsdF4dMw' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=oE4XgICb#1QL0QeVyQv-sUlvbDkhImO1fZGPenxxYj2V8Z5SU9HU' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8080" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:@Asitha2005b@db.waiqbrnuxkjebghzhovz.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL,
