2fb49e5444f2c9aa68508b4cb98cbf1dcf5d21ed
SL-DINU-BOY <139786264+SL-DINU-BOY@users.noreply.github.com>
2025-12-15 07:38:23 +0530

    Update SESSION_ID to include multiple values

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=757f2247bb9db6ba' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=757f2247bb9db6ba,𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=b707c7a813b830c1' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
