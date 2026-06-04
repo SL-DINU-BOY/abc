4989c2d0946e2e41c4bab6ee26dfb7b2dad132f2
SL-DINU-BOY <139786264+SL-DINU-BOY@users.noreply.github.com>
2026-06-04 18:59:43 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=09746e96c7d24b19' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=5a1c5da7b67ccc56' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
